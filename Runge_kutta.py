e00132fe211cc07865efd7a0f6c63c8382e3e843
u1417840 <150192804+u1417840@users.noreply.github.com>
2023-12-10 17:38:50 -0700

    Update Runge_kutta.py

--- Runge_kutta.py
+++ Runge_kutta.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-def rk4(odefun, tspan, x0):
+def rk4(odefun, tspan, y0):
     tLen = len(tspan)
-    xLen = len(x0)
-    xList = np.zeros((tLen, xLen))
-    xList[0, :] = x0
+    yLen = len(x0)
+    yList = np.zeros((tLen, yLen))
+    yList[0, :] = y0
     for i in range(len(tspan) - 1):
         h = tspan[i+1] - tspan[i]
-        k1 = np.array(odefun(tspan[i], xList[i, :]))
-        k2 = np.array(odefun(tspan[i] + 0.5 * h, xList[i, :] + 0.5 * k1 * h))
-        k3 = np.array(odefun(tspan[i] + 0.5 * h, xList[i, :] + 0.5 * k2 * h))
-        k4 = np.array(odefun(tspan[i] + h, xList[i, :] + k3 * h))
+        k1 = np.array(odefun(tspan[i], yList[i, :]))
+        k2 = np.array(odefun(tspan[i] + 0.5 * h, yList[i, :] + 0.5 * k1 * h))
+        k3 = np.array(odefun(tspan[i] + 0.5 * h, yList[i, :] + 0.5 * k2 * h))
+        k4 = np.array(odefun(tspan[i] + h, yList[i, :] + k3 * h))
         slope = (k1 + 2.0*k2 + 2.0*k3 + k4) / 6.0
-        xList[i+1, :] = xList[i, :] + slope * h
-    return tspan, xList
+        yList[i+1, :] = yList[i, :] + slope * h
+    return yList
 
         
 
