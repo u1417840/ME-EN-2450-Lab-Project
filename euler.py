6302602d2c8bccdb9f008e2f58e6163111bb2f46
u1417840 <150192804+u1417840@users.noreply.github.com>
2023-12-11 10:50:18 -0700

    Add files via upload

---
+++ euler.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# change to be of the form
+# [y] = TimeInt(odefun,t,dt,y0,DepFlux_sum(cnt),vine(cnt).mu_L)
+
+def euler(odefun, t, dt, y0, e, g):
+        
+    slope = odefun(t, y0, e, g)
+    
+    return y0 + slope * dt
